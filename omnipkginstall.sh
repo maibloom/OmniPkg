578afe4636102957a8891845933575becca8f288
devtracer <devoft1@gmail.com>
2024-11-08 00:00:36 +0330

    Update omnipkginstall.sh

--- omnipkginstall.sh
+++ omnipkginstall.sh
@@ -1,20 +1,2 @@
-#!/bin/bash
-
-# Check for root privileges
-if [[ "$EUID" -ne 0 ]]; then
-    echo "This script requires root privileges. Please run it as root or with sudo."
-    exit 1
-fi
-
-# Define the installation path
-INSTALL_PATH="/usr/local/bin/omnipkg"
-
-echo "Installing Omnipkg to $INSTALL_PATH..."
-# Move the omnipkg script and version file to the installation path
-if mv omnipkg "$INSTALL_PATH" && mv version.txt "$INSTALL_PATH/version.txt"; then
-    chmod +x "$INSTALL_PATH"
-    echo "Omnipkg installed successfully and is now executable."
-else
-    echo "Installation failed. Could not move Omnipkg to $INSTALL_PATH."
-    exit 1
-fi
+sudo mv omnipkg /usr/local/bin/omnipkg
+sudo chmod +x /usr/local/bin/omnipkg
