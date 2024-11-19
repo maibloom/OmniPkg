a76cc19ebfdff4267857e83fad3e48279e9dc652
devtracer <devoft1@gmail.com>
2024-11-19 23:36:58 +0330

    modified:   omnipkg 	new file:   omnipkg.py

---
+++ omnipkg.py
@@ -0,0 +1,15 @@
+import os
+
+class PackageManagers:
+    def rootenable():
+        os.sudo()
+    def.rootdisable():
+        # goes out of the root mode.
+    def insertcommand
+
+class pacman(PackageManagers):
+    def __init__(self):
+        existence-check = "sudo pacman -v"
+        update-check = "sudo pacman -U"
+        update-database = "sudo pacman -Sy"
+        update-apps = "check for update"
