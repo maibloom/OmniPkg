6bfdca24ab487f6872ebc0f6e9b601848e30f408
Amirali Dashti <devoft1@gmail.com>
2025-05-17 21:35:01 +0330

    Update install.sh

--- packages/google-bro-office/install.sh
+++ packages/google-bro-office/install.sh
@@ -1,4 +1,4 @@
-sudo pacman -S python-pyqt5 python-pyqt5-webengine qt5-webengine qt5-base qt5-svg qt5-declarative qt5-location
+sudo pacman -S python-pyqt5 python-pyqt5-webengine qt5-webengine qt5-base qt5-svg qt5-declarative qt5-location --noconfirm
 
 git clone https://github.com/MaiFOSS/google-bro-office
 
