365aaf8e7bac95ff144b7cfbc909b96a046fafd9
Amirali Dashti <devoft1@gmail.com>
2025-05-17 22:44:56 +0330

    Update install.sh

--- packages/maibloom-builder/install.sh
+++ packages/maibloom-builder/install.sh
@@ -8,59 +8,41 @@ omnipkg put install google-bro-office tuxtalk
 
 sudo pacman -S dialog --noconfirm
 
-# Function to display the dialog menu
-function show_menu {
-    dialog --clear --checklist "Select application categories to install:" 15 50 5 \
-    1 "Development Tools" off \
-    2 "Multimedia" off \
-    3 "Internet" off \
-    4 "Office" off \
-    5 "Games" off 2>tempfile
+# Define the options for the checklist
+OPTIONS=(1 "Education" OFF
+         2 "Programming" OFF
+         3 "Office" OFF
+         4 "Daily Use" OFF
+	     5 "Gaming" OFF
+         )
 
-    # Capture the selected options
-    choices=$(<tempfile)
-    rm tempfile
-}
+# Create the checklist dialog
+CHOICES=$(dialog --clear --title "Let's optimise your experience..." \
+                 --checklist "Which of the following are you going to use your device for?" 15 50 5 \
+                 "${OPTIONS[@]}" \
+                 3>&1 1>&2 2>&3)
 
-# Function to install selected applications
-function install_apps {
-    for choice in $choices; do
-        case $choice in
-            1)
-                apps=("git" "vim" "gcc" "make")
-                ;;
-            2)
-                apps=("vlc" "gimp" "audacity")
-                ;;
-            3)
-                apps=("firefox" "chromium" "telegram-desktop")
-                ;;
-            4)
-                apps=("libreoffice-fresh" "onlyoffice-bin")
-                ;;
-            5)
-                apps=("steam" "0ad" "supertuxkart")
-                ;;
+clear
+
+# Handle the user's selections
+if [[ -z "$CHOICES" ]]; then
+    echo "No option selected."
+else
+    for CHOICE in $CHOICES; do
+        case $CHOICE in
+            1) sudo pacman -Syu gcompris-qt kbruch kgeography kalzium stellarium geogebra tuxmath wxmaxima libreoffice-fresh firefox chromium okular evince --noconfirm ;;
+            2) sudo pacman -Syu base-devel neovim vim code geany kate kwrite clang python nodejs npm jdk-openjdk go rustup cmake ninja maven gradle docker qemu-desktop libvirt virt-manager dnsmasq edk2-ovmf alacritty konsole gnome-terminal gdb valgrind zeal tilix kitty --noconfirm ;;
+            3) sudo pacman -Syu libreoffice-fresh onlyoffice-desktopeditors okular evince zim --noconfirm ;;
+            4) sudo pacman -Syu firefox chromium thunderbird evolution kontact vlc mpv cantata elisa clementine gwenview eog loupe gimp inkscape krita darktable rawtherapee dolphin nautilus thunar pcmanfm pidgin telegram-desktop discord keepassxc flameshot ksnip calibre kdeconnect bleachbit alacritty konsole gnome-terminal tilix kitty --noconfirm ;;
+            5) sudo pacman -Syu steam lutris heroic-games-launcher wine wine-mono wine-gecko retroarch dolphin-emu pcsx2 mangohud lib32-mangohud gamemode lib32-gamemode corectrl gwe discord mumble vulkan-radeon lib32-vulkan-radeon vulkan-intel lib32-vulkan-intel --noconfirm ;;
+	    *) echo "Invalid option" ;;
         esac
     done
+fi
 
-    # Install the selected applications
-    if [ -n "$apps" ]; then
-        dialog --title "Installing Applications" --infobox "Installing selected applications..." 5 40
-        sleep 2  # Simulate installation time
-        sudo pacman -S --noconfirm "${apps[@]}"
-        dialog --msgbox "Applications installed successfully!" 5 40
-    else
-        dialog --msgbox "No applications selected." 5 40
-    fi
-}
-
-show_menu
-install_apps
 
 
 
 dialog --title "Installation Successful" --msgbox "You have successfully installed the OS.\n\nPlease unplug the USB drive and reboot your system." 10 50
 
-closed
 reboot
