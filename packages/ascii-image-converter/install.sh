b5cd11f94035ab25b900a05331e5f69236ed274e
Amirali Dashti <devoft1@gmail.com>
2025-05-18 19:21:59 +0000

    Update install.sh

--- packages/ascii-image-converter/install.sh
+++ packages/ascii-image-converter/install.sh
@@ -6,9 +6,9 @@ makepkg -si --noconfirm
 
 
 if command -v yay &> /dev/null; then
-  yay -S ascii-image-converter-git
+  yay -S ascii-image-converter-git --noconfirm
 else
   echo "yay is not installed. Using OmniPkg to install yay..."
   omnipkg put install yay
-  yay -S ascii-image-converter-git
+  yay -S ascii-image-converter-git --noconfirm
 fi
