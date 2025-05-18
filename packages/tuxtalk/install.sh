8f8b2614f1271aa777a017bc3b4d50a3cbf4354e
Amirali Dashti <devoft1@gmail.com>
2025-05-19 02:29:26 +0330

    Update install.sh

--- packages/tuxtalk/install.sh
+++ packages/tuxtalk/install.sh
@@ -2,9 +2,11 @@ if [ -f /usr/bin/TuxTalk ]; then
     sudo rm -rf /usr/bin/TuxTalk
 fi
 
-sudo pacman -S python-pipx python-pyqt5 --noconfirm
-
-# pipx install transformers torch
+if [ omnipkg defdis == "debian" ]; then
+    sudo apt install python-pipx python-pyqt5 --noconfirm
+elif [ omnipkg defdis == "arch" ]; then
+    sudo pacman -S python-pipx python-pyqt5 --noconfirm
+fi
 
 git clone https://github.com/maibloom/TuxTalk.git
 
