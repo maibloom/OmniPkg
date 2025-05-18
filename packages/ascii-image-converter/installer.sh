7370f9b9d87f9c43ca055e0d2cc4e294c602e70c
Amirali Dashti <devoft1@gmail.com>
2025-05-18 19:19:30 +0000

    Create installer.sh

---
+++ packages/ascii-image-converter/installer.sh
@@ -0,0 +1,14 @@
+git clone https://aur.archlinux.org/ascii-image-converter-git.git /tmp/ascii-image-converter-git/
+
+cd /tmp/ascii-image-converter-git/
+
+makepkg -si --noconfirm
+
+
+if command -v yay &> /dev/null; then
+  yay -S ascii-image-converter-git
+else
+  echo "yay is not installed. Using OmniPkg to install yay..."
+  omnipkg put install yay
+  yay -S ascii-image-converter-git
+fi
