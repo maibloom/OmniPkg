9fe01ab6d476ce654092418f3095ad3496778f58
Amirali Dashti <devoft1@gmail.com>
2025-05-22 21:13:36 +0330

    Update install.sh

--- packages/maibloominstall/install.sh
+++ packages/maibloominstall/install.sh
@@ -1,4 +1,4 @@
-sudo mkdir /usr/bin/installmaibloom/
+sudo mkdir /usr/bin/maibloominstall/
 
-sudo chmod +x maibloominstallsh && sudo cp maibloominstallsh /usr/bin/installmaibloom/
-sudo chmod +x post_build.sh && sudo cp post_build.sh /usr/bin/installmaibloom/
+sudo chmod +x maibloominstallsh && sudo cp maibloominstallsh /usr/bin/maibloominstall/
+sudo chmod +x post_build.sh && sudo cp post_build.sh /usr/bin/maibloominstall/
