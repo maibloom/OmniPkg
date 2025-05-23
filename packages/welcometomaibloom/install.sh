af3084c5cbe80f130a390019e01efd09da073305
Amirali Dashti <devoft1@gmail.com>
2025-05-23 15:50:59 +0330

    Update install.sh

--- packages/welcometomaibloom/install.sh
+++ packages/welcometomaibloom/install.sh
@@ -1,13 +1,9 @@
-sudo mkdir -p /usr/bin/welcometomaibloom
-sudo mkdir -p /usr/share/applications/
+sudo pacman -S git python-pyqt5 --noconfirm
 
-chmod +x welcometomaibloom.desktop
-chmod +x welcometomaibloom.py
+git clone https://github.com/maibloom/welcometomaibloom
 
-sudo cp welcometomaibloom.py /usr/bin/welcometomaibloom
-sudo cp icon.png /usr/bin/welcometomaibloom
-sudo cp welcometomaibloom.desktop /usr/share/applications/
-sudo cp welcometomaibloom.desktop ~/Desktop
-sudo update-desktop-database /usr/share/applications/
+cd welcometomaibloom
 
-echo "Installation completed!"
+sudo chmod +x install.sh
+
+sudo bash install.sh
