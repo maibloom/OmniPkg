7a9702f386334545475e2f87dfefa45abe3e0b8a
Amirali Dashti <devoft1@gmail.com>
2025-05-23 17:25:04 +0330

    Update remove.sh

--- packages/welcometomaibloom/remove.sh
+++ packages/welcometomaibloom/remove.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 
 sudo rm /usr/share/applications/welcometomaibloom.desktop
-sudo rm -rf /usr/local/bin/welcometomaibloom
+sudo rm -rf /usr/local/bin/welcometomaibloom/
 
 # Update the desktop database to reflect the removal
 sudo update-desktop-database /usr/share/applications
