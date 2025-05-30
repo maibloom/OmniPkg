072f407ce3d475ade2db9e4488faa3e3d35b5891
Amirali Dashti <devoft1@gmail.com>
2025-05-30 16:32:02 +0330

    Update install.sh

--- packages/maibloom-gamingpackage/install.sh
+++ packages/maibloom-gamingpackage/install.sh
@@ -1,3 +1,5 @@
 sudo omnipkg put install multilib-mirror
 
+sudo pacman -S phonon-qt6-mpv --noconfirm
+
 sudo pacman -S --noconfirm steam lutris wine winetricks gamemode mangohud retroarch discord vulkan-icd-loader lib32-vulkan-icd-loader
