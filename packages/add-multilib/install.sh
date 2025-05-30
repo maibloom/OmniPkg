620e3f484d40e7d70c64374691e4e3dc52ab944a
Amirali Dashti <devoft1@gmail.com>
2025-05-30 15:24:46 +0330

    Update install.sh

--- packages/add-multilib/install.sh
+++ packages/add-multilib/install.sh
@@ -3,4 +3,6 @@ grep -qxF "[multilib]" /etc/pacman.conf || sudo tee -a /etc/pacman.conf << 'EOF'
 Include = /etc/pacman.d/mirrorlist
 EOF
 
+sudo pacman -Syu --noconfirm
+
 echo "Multilib has been added successfully"
