8bf9cf791c93034a2fb230112eb3df3e38fdc09b
Amirali Dashti <devoft1@gmail.com>
2025-05-30 16:10:50 +0330

    Update install.sh

--- packages/maibloom-gamingpackage/install.sh
+++ packages/maibloom-gamingpackage/install.sh
@@ -1,3 +1,3 @@
-sudo omnipkg put install add-multilib
+sudo omnipkg put install multilib-mirror
 
 sudo pacman -S --noconfirm steam lutris wine winetricks gamemode mangohud retroarch discord vulkan-icd-loader lib32-vulkan-icd-loader
