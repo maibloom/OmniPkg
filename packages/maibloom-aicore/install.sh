0e77b509ad62ee397b4921226cf9bc5c35f65635
Amirali Dashti <devoft1@gmail.com>
2025-05-30 20:41:27 +0330

    Update install.sh

--- packages/maibloom-aicore/install.sh
+++ packages/maibloom-aicore/install.sh
@@ -6,8 +6,8 @@ git clone https://www.github.com/maibloom/maibloom-aicore
 
 cd maibloom-aicore
 
-chmod +x maibloom-aicore.py
+chmod +x *
 
-sudo cp maibloom-aicore.py /usr/local/bin/maibloom-aicore
+sudo cp * /usr/local/bin/maibloom-aicore
 
 maibloom-aicore "Say hi to the user!" --verbose
