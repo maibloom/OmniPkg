20b3b493e1adf4c5652d8dcf3abbcc3756d2e86e
Amirali Dashti <devoft1@gmail.com>
2025-05-30 16:07:24 +0330

    Update install.sh

--- packages/add-bioarchlinux/install.sh
+++ packages/add-bioarchlinux/install.sh
@@ -3,9 +3,9 @@ grep -qxF "[bioarchlinux]" /etc/pacman.conf || sudo tee -a /etc/pacman.conf << '
 Server = https://repo.bioarchlinux.org/$arch
 EOF
 
-pacman-key --recv-keys B1F96021DB62254D
-pacman-key --finger B1F96021DB62254D
-pacman-key --lsign-key B1F96021DB62254D
+sudo pacman-key --recv-keys B1F96021DB62254D
+sudo pacman-key --finger B1F96021DB62254D
+sudo pacman-key --lsign-key B1F96021DB62254D
 
 sudo pacman -Syu --noconfirm
 
