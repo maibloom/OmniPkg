2363f67f25743b809f97694114bb85ea3fcaaeb0
Amirali Dashti <devoft1@gmail.com>
2025-05-30 20:33:42 +0330

    Update install.sh

--- packages/maibloom-aicore/install.sh
+++ packages/maibloom-aicore/install.sh
@@ -1,3 +1,13 @@
 omnipkg put install pypippark
 
-pypippark install tensorflow transformers
+sudo pypippark install transformers torch
+
+git clone https://www.github.com/maibloom/maibloom-aicore
+
+cd maibloom-aicore
+
+chmod +x maibloom-aicore.py
+
+sudo cp maibloom-aicore.py /usr/local/bin/maibloom-aicore
+
+maibloom-aicore "Say hi to the user!" --verbose
