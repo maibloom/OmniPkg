126c2581239dfc131cb69d18872b4345f0d8197d
Amirali Dashti <devoft1@gmail.com>
2025-06-02 23:07:23 +0330

    Update install.sh

--- packages/maibloom-edupackage/install.sh
+++ packages/maibloom-edupackage/install.sh
@@ -1,3 +1,3 @@
 sudo pacman -S phonon-qt6-mpv --noconfirm
 
-sudo pacman -S cantor kig ktouch kturtle kwordquiz parley marble --noconfirm
+sudo pacman -S cantor kig ktouch kturtle kwordquiz parley marble --needed --noconfirm
