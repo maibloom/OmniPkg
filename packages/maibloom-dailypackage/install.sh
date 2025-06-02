a56c87b9df7e0069a22ba5a1f89e10b94ac63cf8
Amirali Dashti <devoft1@gmail.com>
2025-06-02 23:20:39 +0330

    Update install.sh

--- packages/maibloom-dailypackage/install.sh
+++ packages/maibloom-dailypackage/install.sh
@@ -1,2 +1,2 @@
 sudo pacman -S phonon-qt6-mpv --noconfirm
-sudo pacman -S --needed --noconfirm gnome-terminal nautilus gedit htop rofi vlc
+sudo pacman -S --noconfirm gnome-terminal nautilus gedit htop rofi vlc
