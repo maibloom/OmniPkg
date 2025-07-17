9aec48b7539656ea97332244499772dfe5c8ab30
Amirali Dashti <devoft1@gmail.com>
2025-07-17 22:29:46 +0330

    Update install.sh

--- packages/cosmic/install.sh
+++ packages/cosmic/install.sh
@@ -1,15 +1,15 @@
-sudo pacman -S cosmic --noconfirm
+#!/usr/bin/env bash
+set -euo pipefail
 
-sudo pacman -S cosmic-session --noconfirm
+sudo pacman -Syu --noconfirm
+
+sudo pacman -S cosmic --noconfirm
 
 sudo pacman -S gdm --noconfirm
 sudo systemctl enable gdm.service
-sudo pacman -S cosmic-greeter --noconfirm
-sudo systemctl enable cosmic-greeter.service
 
-sudo pacman -S packagekit power-profiles-daemon xdg-user-dirs --noconfirm
+sudo pacman -S packagekit power-profiles-daemon xdg-user-dirs gnome-keyring --noconfirm
 sudo systemctl enable power-profiles-daemon.service
 xdg-user-dirs-update
 
-sudo pacman -S gnome-keyring --noconfirm
 sudo pacman -S cosmic-text-editor cosmic-player --noconfirm
